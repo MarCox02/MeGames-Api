cca30707fefdeebaaa9a58326f425df312c82eb2
MarCox02 <Cox.Martin2002@gmail.com>
2024-06-04 18:34:00 -0400

    Definitivamente no me robe esto

---
+++ controllers/games.controller.js
@@ -0,0 +1,38 @@
+const { request, response } = require("express");
+const { games } = require("../games");
+
+const getGames = (req, res)=> {
+    return res.json({
+        ok:true,
+        statusCode:200,
+        games
+    })
+}
+
+const getGameById = (req = request, res = response)=> {
+
+    let id = parseInt(req.params.id);
+    let gameABuscar = "";
+    gameABuscar = games.find((juego)=> {
+        return juego.id === id;
+    });
+
+    if(gameABuscar) {
+        return res.json({
+            ok:true,
+            statusCode:200,
+            gameABuscar
+        });
+    } else {
+        return res.json({
+            ok:true,
+            statusCode:404,
+            msg:"no hay jogo"
+        });
+    }
+}
+
+module.exports = { 
+    getGames,
+    getGameById
+}
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const puerto = 2005;
+
+const { getGames, getGameById } = require("./controllers/games.controller")
+
+app.use(cors());
+app.get("/", getGames);
+app.get("/:id", getGameById);
+
+app.listen(puerto, ()=> {
+    console.log(`Servidor OK. Puerto: ${puerto}`);
+});
